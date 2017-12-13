3eb840030f3b6c6aff55b25134417b519a34f84b
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-13 18:17:52 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,20 @@
 class ArtistsController < ApplicationController
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(artist_params)
+    redirect_to artist_path
+  end
+  
   def show
     @artist = Artist.find(params[:id])
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(name: params[:name], bio: params[:bio])
   end
 end
